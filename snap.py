a0593e01abc4ec477fad3a53c605a6526fa67462
Bernardo Loureiro <bernardo.lou@gmail.com>
2023-06-17 13:22:24 -0300

    Checking if package is already installed

--- snap.py
+++ snap.py
@@ -10,6 +10,13 @@ class Snap(dotbot.Plugin):
     def can_handle(self, directive: str):
         return self._directive == directive
 
+    def _is_installed(self, name: str):
+        cmd = f'snap list | grep -c \'^{name}\\s\''
+        result = subprocess.run(
+            cmd, shell=True, check=False, stdout=subprocess.DEVNULL,
+            stderr=subprocess.STDOUT)
+        return result.returncode == 0
+
     def handle(self, directive: str, data: List):
         if directive != self._directive:
             raise ValueError(f'snap cannot handle directive {directive}')
@@ -34,11 +41,20 @@ class Snap(dotbot.Plugin):
                         is_classic = options.get('classic', classic_defauls)
 
             if is_classic:
-                commands.append(f'{command_prefix} --classic {name}')
+                if self._is_installed(name):
+                    print(f'classic {name} is already installed')
+                else:
+                    commands.append(f'{command_prefix} --classic {name}')
             else:
                 apps.append(name)
 
-        app_list = ' '.join(apps)
+        app_list = ''
+        for app in apps:
+            if self._is_installed(app):
+                print(f'{app} is already installed')
+            else:
+                app_list += f' {app}'
+
         if app_list:
             commands.append(f'{command_prefix} {app_list}')
 
